c7a68c93c945cce7692c828ed6390d576f7ba1c9
Bowen WU <bowenbhwu@gmail.com>
2022-01-01 23:08:29 +0800

    Docs sync for the lexer

--- Lexer/lexer.h
+++ Lexer/lexer.h
@@ -9,4 +9,23 @@
 #include "dfa.h"
 
 int get_char_index(char c);
-int next_token(char** ptr, int* token_idx);
+/*
+ function:
+	Get the index of a character
+ argunment:
+	c: The character to be indexed
+ return:
+	The index of the character in the DFA transition table.
+	A -1 will be returned if the character is not found.
+*/
+
+int next_token(char** ptr);
+/*
+ function:
+	Get the next token
+ argunment:
+	ptr: The address of the variable that stores the input string
+	to
+ return:
+	true if the stack is initialized correctly
+*/
