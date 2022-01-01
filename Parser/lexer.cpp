22301b2ff45ca3d238809b4e5005663b4c3bb3db
Bowen WU <bowenbhwu@gmail.com>
2022-01-01 20:24:51 +0800

    New lexer API

--- Parser/lexer.cpp
+++ Parser/lexer.cpp
@@ -8,35 +8,35 @@ int get_char_index(char c)
     return -1;
 }
 
-char* next_token(char *ptr, int *token_idx)
+int next_token(char** ptr, int* token_idx)
 {
     int current_state = 0;
     int col = -1;
     char current_char;
-    while (*ptr != '\n' && *ptr != '\0') {
-        current_char = *ptr;
+    while (*(*ptr) != '\n' && *(*ptr) != '\0') {
+        current_char = **ptr;
 
         // If the char_index is -1, it indicates the char is not in the alphabet
         col = get_char_index(current_char);
-        if (col < 0) {
-            *token_idx = -1;
-            return NULL;
-        }
+        if (col < 0)
+            return -1;
 
         // Jump according to the transition table
         current_state = trans_table[current_state][col];
 
+        if (current_state < 0)
+            return -1;
+
         // Check for whether it is in final states
         for (int i = 0; i < num_accepted_states; i++) {
             if (current_state == accepted_states[i]) {
-                *token_idx = i;
-                return ptr+1;
+                (*ptr)++;
+                return i;
             }
         }
 
-        ptr++;
+        (*ptr)++;
     }
-    *token_idx = -1;
-    return NULL;
+    return -1;
 }
 
